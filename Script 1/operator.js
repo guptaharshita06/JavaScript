1be14e338791d39d182d34c811b963f33df872fc
Harshita Gupta <guptaharshitastar06@gmail.com>
2025-07-01 21:02:50 +0530

    Create operator.js

---
+++ Script 1/operator.js
@@ -0,0 +1,37 @@
+//operators
+
+//Arthimetic operators
+const add = 5+2;
+const sub = 5-2;
+const mul = 5*2;
+const div = 5/2;
+console.log(add , sub, mul, div);
+
+//Assignment operators
+let a = 5;
+a += 2;
+a *= 2;
+console.log(a);
+
+//Logical Operators
+const c = true, b = false;
+console.log(c && b);
+console.log(c || b);
+
+//Bitwisw Operators
+const d = 1, m = 5;
+console.log(d & m);
+
+//comma operator
+let g=9,h=5;
+let result = (g=9, h=5, g+h);
+console.log(result);
+
+//string operators
+let s1= "Hello";
+console.log(s1);
+
+//Unary operators
+let z = 4;
+console.log(++z);
+console.log(z--);
