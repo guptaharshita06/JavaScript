8d5b6eebb099f6cc290d5a3c9b616a230edd4895
Harshita Gupta <guptaharshitastar06@gmail.com>
2025-07-12 21:12:56 +0530

    Create objectconstructor.js

---
+++ Script 1/objectconstructor.js
@@ -0,0 +1,9 @@
+function Person(first, last, age, eyecolor) {
+  this.firstName = first;
+  this.lastName = last;
+  this.age = age;
+  this.eyeColor = eyecolor;
+  this.fullName = function() {
+    return this.firstName + " " + this.lastName;
+  };
+}
